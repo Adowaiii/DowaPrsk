e43928cb6f7b290026feb253c8ae75e51af54e0b
JuliaChen4061 <50419julia@gmail.com>
2023-01-27 00:20:54 +0800

    [0127] Add reading time estimate

---
+++ docs/posts/main.js
@@ -0,0 +1,12 @@
+function readingTime() {
+    const text = document.getElementById("quarto-document-content").innerText;
+    const wpm = 500;
+    var words = text.replace(/[a-zA-Z \n。，、？「」!！\s・/—….]/g, "");
+    words = words.trim().split("").length;
+    const time = Math.ceil(words / wpm);
+    
+    document.getElementById("time").innerText = time;
+  }
+
+
+  readingTime();
