2ca9727bda84ec8ca1a65b5805c0849f5ec41d2c
JuliaChen4061 <50419julia@gmail.com>
2023-03-20 04:57:57 +0800

    [0320] New Post

--- docs/posts/main.js
+++ docs/posts/main.js
@@ -1,7 +1,7 @@
 function readingTime() {
     const text = document.getElementById("quarto-document-content").innerText;
     const wpm = 500;
-    var words = text.replace(/[a-zA-Z \n。，、？「」!！\s・/—….]/g, "");
+    var words = text.replace(/[a-zA-Z \n。，、？「」!！\s・/—….<>]/g, "");
     words = words.trim().split("").length;
     const time = Math.ceil(words / wpm);
     
